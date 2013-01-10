5bf1b4c25686288399be99d6935b63e5e98a3d87
Nyashechka <v.s.korkosh@gmail.com>
2013-01-10 22:30:11 +0400

    Update js/game.js

--- js/game.js
+++ js/game.js
@@ -7,9 +7,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     var canvas = document.getElementById('main-canvas');
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#FF0000';
+    ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     GameController.init();
     var monster = new Creature();
+    var Hero = new Hero();
     console.log(monster);
 });
